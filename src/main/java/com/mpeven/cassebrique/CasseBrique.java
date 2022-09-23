7b4ae9fe5aeb9aa1c5a766e3bfd31729c5c0874d
54maria-pia.even <mariapianavach@gmail.com>
2022-09-23 16:53:35 +0200

    Initial commit

---
+++ src/main/java/com/mpeven/cassebrique/CasseBrique.java
@@ -0,0 +1,66 @@
+package com.mpeven.cassebrique;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CasseBrique extends Canvas {
+
+    public CasseBrique() throws InterruptedException {
+        JFrame fenetre = new JFrame("Casse brique");
+        //On récupère le panneau de la fenetre principale
+        JPanel panneau = (JPanel) fenetre.getContentPane();
+        //On définie la hauteur / largeur de l'écran
+        panneau.setPreferredSize(new Dimension(500, 500));
+        setBounds(0, 0, 500, 500);
+        //On ajoute cette classe (qui hérite de Canvas) comme composant du panneau principal
+        panneau.add(this);
+
+        fenetre.pack();
+        fenetre.setResizable(false);
+        fenetre.setLocationRelativeTo(null);
+        fenetre.setVisible(true);
+        fenetre.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        fenetre.requestFocus();
+
+        //On indique que le raffraichissement de l'ecran doit être fait manuellement.
+        createBufferStrategy(2);
+        setIgnoreRepaint(true);
+        this.setFocusable(false);
+
+        demarrer();
+    }
+
+    public void demarrer() throws InterruptedException {
+
+        long indexFrame = 0;
+        int xBalle = 500;
+        int vitesseHorizontalBalle = -2;
+
+
+        while (true) {
+            indexFrame++;
+
+            Graphics2D dessin = (Graphics2D) getBufferStrategy().getDrawGraphics();
+
+            //-------------------------------
+            //Reset dessin
+            dessin.setColor(Color.WHITE);
+            dessin.fillRect(0, 0, 500, 500);
+
+            //-------------------------------
+            //Dessin Ballon
+            xBalle += vitesseHorizontalBalle;
+            dessin.setColor(Color.MAGENTA);
+            dessin.fillOval(xBalle, 250, 30, 30);
+
+            //-------------------------------
+            dessin.dispose();
+            getBufferStrategy().show();
+            Thread.sleep(1000 / 60);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new CasseBrique();
+    }
+}
